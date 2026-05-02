0996af780b78bb061d38f65eeba42dff563f66fe
Hariprasath A U <hari24prasath2005@gmail.com>
2026-05-02 13:56:49 +0530

    Fix frontend API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://taskflow-5x94.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
